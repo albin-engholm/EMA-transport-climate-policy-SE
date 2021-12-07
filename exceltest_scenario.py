4f1b148b08691953bd6f14b5e0d0e9b404215d8e
Albin <aengholm@kth.se>
2021-12-07 21:59:44 +0100

    Added previous EMA testing files for Trafikverket's scenario tool

---
+++ exceltest_scenario.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 12:45:55 2021
+
+@author: aengholm
+This is a script for setting up and running an EMA analysis using a static 
+excel model with parametric uncertainties(designed for the TRV scenario model)
+Current version has not implement policies for the specific model
+Results are saved and can be loaded for analysis in separate script - 
+e.g. scenario_exploration_excel_static.py'
+""" 
+import numpy as np
+import pandas as pd
+from ema_workbench import (RealParameter, TimeSeriesOutcome, ScalarOutcome, ema_logging,
+                           perform_experiments)
+
+from ema_workbench.connectors.excel import ExcelModel
+from ema_workbench.em_framework.evaluators import MultiprocessingEvaluator
+if __name__ == "__main__":
+    ema_logging.log_to_stderr(level=ema_logging.INFO)
+
+    #Set model
+    model = ExcelModel("scenarioModel", wd="./models",
+                       model_file='trv_scenario2.xlsx')
+    model.default_sheet = "Indata"
+    
+    #Set parametric uncetainties
+    model.uncertainties = [RealParameter("F10", 0, 0.7)  
+                          ,RealParameter("F12", 0.25, 0.80)
+                          ,RealParameter("F15", 0,1)
+                          ,RealParameter("F16", 0,1)
+                          ,RealParameter("D79",.05,0.7)
+                          ,RealParameter("F61",-0.3,0.30)
+                          ,RealParameter("F28",0.1,0.7)
+                          ,RealParameter("F38",-0.3,0.3)
+                          ]
+    # copy model to use for worst case discovery (with sub set of outcomes)
+    # specification of the outcomes
+    model.outcomes = [ScalarOutcome("D66"),
+                      ScalarOutcome("D67"),
+                      ScalarOutcome("D68"),
+                      ScalarOutcome("D69"),
+                      ScalarOutcome("D70"),
+                      ScalarOutcome("D71"),
+                      ScalarOutcome("D72"),
+                      ScalarOutcome("D73"),
+                      ScalarOutcome("D74"),
+                      ScalarOutcome("D75")
+                      ]  
+    
+    # Add policies
+    # policies = [Policy('Low ambition',
+    #                    model_file=r'FLUvensimV1basecase.vpm'),
+    #             Policy('High ambition',
+    #                    model_file=r'FLUvensimV1static.vpm'),
+    #                      ]
+    policies=[]
+       
+    #%%
+    #select number of scenarios (per policy)
+    nr_scenarios=1000
+    
+    #Run model - for open exploration
+    import time
+    tic=time.perf_counter()
+    run_with_policies=0
+    if run_with_policies == 1:
+        experiments, outcomes = perform_experiments(model, nr_scenarios,policies=policies)
+    else:
+        ### Run model using multiprocessing
+        use_multi=1
+        if use_multi == 1:
+            n_p=3
+            with MultiprocessingEvaluator(msis=model,n_processes=n_p) as evaluator:
+                experiments, outcomes = perform_experiments(model, 
+                                                            nr_scenarios, 
+                                                            reporting_frequency=100, 
+                                                            evaluator=evaluator)
+        else:
+            experiments, outcomes = perform_experiments(model, nr_scenarios, 
+                                                        reporting_frequency=100)
+            
+    toc=time.perf_counter()
+    print("Runtime [s]= " +str(toc-tic))
+    print("Runtime [h]= " +str(round((toc-tic)/3600,1)))
+    print("Runtime per experiment [s]= " +str((toc-tic)/len(experiments)))
+    
+    #%%
+    #Run model for wost case discovery
+    run_worstcase=0
+    if run_worstcase ==1:
+        
+        model_worstcase=model
+        model_worstcase.outcomes.clear()
+        # specification of the outcomes forworst case. Set desirable direction
+        model_worstcase.outcomes = [ScalarOutcome("D66", ScalarOutcome.MINIMIZE),
+                                    ScalarOutcome("D67",ScalarOutcome.MINIMIZE),
+                                    ScalarOutcome("D79",ScalarOutcome.MINIMIZE)
+                                    ]  
+        
+        # change outcomes so direction is undesirable
+        minimize = ScalarOutcome.MINIMIZE
+        maximize = ScalarOutcome.MAXIMIZE
+        for outcome in model_worstcase.outcomes:
+            if outcome.kind == minimize:
+                outcome.kind = maximize
+            else:
+                outcome.kind = minimize
+        ## Run worstcase scenario
+        from ema_workbench.em_framework.optimization import (HyperVolume,
+                                                     EpsilonProgress)
+
+        convergence_metrics = [HyperVolume(minimum=[0,0,0,0], maximum=[1,1.01,1.01,1.01]),
+                       EpsilonProgress()]
+        with MultiprocessingEvaluator(model_worstcase) as evaluator:
+            results_worstcase, convergence = evaluator.optimize(nfe=200, searchover='uncertainties',
+                                         epsilons=[0.1,]*len(model_worstcase.outcomes),
+                                         convergence=convergence_metrics)
+        #%%
+        from ema_workbench.analysis import parcoords
+        import matplotlib.pyplot as plt
+
+
+        fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True, figsize=(8,4))
+        ax1.plot(convergence.nfe, convergence.epsilon_progress)
+        ax1.set_ylabel('$\epsilon$-progress')
+        ax2.plot(convergence.nfe, convergence.hypervolume)
+        ax2.set_ylabel('hypervolume')
+        
+        ax1.set_xlabel('number of function evaluations')
+        ax2.set_xlabel('number of function evaluations')
+        plt.show()
+        data = results_worstcase.loc[:, [o.name for o in model_worstcase.outcomes]]
+        data = data.iloc[: , :-1]
+        limits = parcoords.get_limits(data)
+        limits.loc[0, ['D66', 'D67', 'D79']] = 0
+        limits.loc[1, ['D66', 'D67', 'D79']] = 1
+        
+        paraxes = parcoords.ParallelAxes(limits)
+        paraxes.plot(data)
+        #paraxes.invert_axis('max_P')
+        plt.show()
+
+     
+     #%%
+
+    #Rename outcome variables
+    outcomes["CO2 change truck"] = outcomes.pop("D66")
+    outcomes["CO2 change tot"] = outcomes.pop("D67")
+    outcomes["VKT trucks"] = outcomes.pop("D68")
+    outcomes["VKT tot"] = outcomes.pop("D69")
+    outcomes["Energy tot"] = outcomes.pop("D70")
+    outcomes["Energy bio"] = outcomes.pop("D71")
+    outcomes["Energy fossile"] = outcomes.pop("D72")
+    outcomes["Energy el"] = outcomes.pop("D73")
+    outcomes["Total cost trucks"] = outcomes.pop("D74")
+    outcomes["Total costs cars"] = outcomes.pop("D75")
+    #Rename input variables
+    experiments["share HVO gas."] = experiments.pop("F10")
+    experiments["share HVO diesel"] = experiments.pop("F12")
+    experiments["km-tax light veh"] = experiments.pop("F15")
+    experiments["km-tax trucks"] = experiments.pop("F16")
+    experiments["Heavy truck el. share"] = experiments.pop("D79")
+    experiments["Truck demand change"] = experiments.pop("F61")
+    experiments["Car demand change"] = experiments.pop("F38")
+    experiments["Car el share"] = experiments.pop("F28")
+    
+    
+         # Save results?
+    save_results=1
+    if save_results==1:
+        from datetime import date    
+        from ema_workbench import save_results
+        filename=str(nr_scenarios)+'_scenarios_'+str(date.today())+'.tar.gz'
+        save_results([experiments,outcomes], filename)
+        #df.to_hdf('data.h5', key='df', mode='w')    
+
+
---
+++ scenario_exploration_excel_static_CBA_test.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 10:20:38 2021
+
+@author: aengholm
+"""
+
+import numpy as np
+import pandas as pd
+from ema_workbench import (RealParameter, TimeSeriesOutcome, ScalarOutcome, ema_logging,
+                           perform_experiments)
+
+import matplotlib.pyplot as plt
+SMALL_SIZE = 6
+MEDIUM_SIZE = 8
+BIGGER_SIZE = 10
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rcParams['figure.dpi'] = 300
+
+from ema_workbench.analysis.pairs_plotting import (pairs_lines, pairs_scatter,
+                                               pairs_density)
+from ema_workbench.analysis import feature_scoring
+import seaborn as sns
+sns.set(rc={"figure.dpi":300})
+
+#Should previously saved result data be loaded? If not, data from workspace is used
+load_results=1
+if load_results==1:
+    from ema_workbench import load_results
+    results = load_results('20000_scenarios_2021-11-30.tar.gz')
+    experiments=results[0]
+    outcomes=results[1]
+
+### SCENARIO EXPLORATION###
+
+fig,axes = pairs_scatter(experiments,outcomes, legend=False)
+fig.set_size_inches(15,15)
+plt.show()
+
+
+# plt.xticks(rotation=45) #rotate x-axis labels by 45 degrees.
+# plt.yticks(rotation=45) #rotate y-axis labels by 90 degrees.
+sns.set_palette("deep")
+### Show hist/KDE of 
+
+# plt.axvline(-0.7, color='r', linestyle='dashed', linewidth=1)
+# plt.axhline(20, color='r', linestyle='dashed', linewidth=1)
+
+###FEATURE SCORING ON OUTCOMES
+
+
+
+fs = feature_scoring.get_feature_scores_all(experiments, outcomes)
+plt.figure()
+fig=sns.heatmap(fs, cmap='viridis', annot=True,fmt=".2f")
+
+
+
+### scenario discovery
+
+#Define criterion for unwanted outcome
+fail_criterion_CO2=-0.7
+fail_criterion_bio=20
+#Prepare data, x and y arrays
+x = experiments
+y1=outcomes['CO2 change tot']>fail_criterion_CO2
+y2=outcomes['Energy bio']>fail_criterion_bio
+y3=[0]*len(x)
+for j in range(len(y1)):
+    if y1[j]==True or y2[j]==True:
+        y3[j]=True
+    else: 
+        y3[j]=False
+#choose criterion to use y1=first criterion only, y2=second criterion only,y3=either first or second criterion=fail
+y=np.array(y3,dtype=bool)
+
+
+#Basic statistics 
+n_fail = np.count_nonzero(y)
+share_fail=n_fail/len(y)
+share_success=1-share_fail
+
+import statistics
+#Plot hist/KDE on criterions
+sns.displot(x='CO2 change tot', data=outcomes, kde=True)
+plt.axvspan(fail_criterion_CO2, max(outcomes['CO2 change tot']), facecolor='red', alpha=0.2,edgecolor='None')
+plt.axvline(statistics.mean(outcomes['CO2 change tot']),color="red")
+
+sns.displot(x='Energy bio', data=outcomes, kde=True)
+plt.axvspan(fail_criterion_bio, max(outcomes['Energy bio']), facecolor='red', alpha=0.2,edgecolor='None')
+plt.axvline(statistics.mean(outcomes['Energy bio']),color="red")
+
+g=sns.displot(x='CO2 change tot', y='Energy bio', data=outcomes)
+ylim=g.ax.get_ylim()
+xlim=g.ax.get_xlim()
+plt.axvspan(fail_criterion_CO2, xlim[1],facecolor='red', alpha=0.2, edgecolor='none')
+plt.axvspan(xlim[0],fail_criterion_CO2,fail_criterion_bio/ylim[1],facecolor='red', alpha=0.2,edgecolor='none')
+
+
+
+
+# Feature scoring on scenario disocvery data (what ucnertainties drive fail/success of outcomes)
+fs_discovery, alg = feature_scoring.get_ex_feature_scores(x, y, mode=feature_scoring.RuleInductionType.CLASSIFICATION)
+fs_discovery.sort_values(ascending=False, by=1)
+plt.figure()
+fig=sns.heatmap(fs_discovery, cmap='viridis', annot=True)
+
+#Scatter pair plot for all uncertainties and whether or not target is met
+x_copy2 = experiments.copy()
+x_copy2= x_copy2.drop('scenario', axis=1)
+x_copy2["Target not met"]=y
+g= sns.PairGrid(x_copy2, hue="Target not met")
+g.map_diag(sns.kdeplot, shade=True)
+g.map_offdiag(plt.scatter,edgecolor="white",alpha=0.5)
+g.add_legend()
+g.fig.set_size_inches(20,20)
+
+#Dimensional stacking
+from ema_workbench.analysis import dimensional_stacking
+dimensional_stacking.create_pivot_plot(x,y, 2, nbins=5)
+plt.show()
+
+#Regional sensitivty analysis, # model is the same across experiments
+from ema_workbench.analysis import regional_sa
+from numpy.lib import recfunctions as rf
+sns.set_style('white')
+x_copy = experiments.copy()
+x_copy = x_copy.drop('model', axis=1)
+x_copy = x_copy.drop('policy', axis=1)
+fig = regional_sa.plot_cdfs(x_copy,y,ccdf=False)
+sns.despine()
+plt.show()
+
+#Perform PRIM analysis
+from ema_workbench.analysis import prim
+prim_alg = prim.Prim(x, y, threshold=0.5)
+#Find 1st box
+box1 = prim_alg.find_box()
+#%%%
+#Visualizations of Box1
+box1.show_tradeoff()
+
+for i in range(0,len(box1.peeling_trajectory.T.columns)):
+    s=box1.peeling_trajectory.T[i].id
+    if (i%2)==0:
+        plt.text(box1.peeling_trajectory.T[i].coverage+.02,box1.peeling_trajectory.T[i].density+.03 ,s,fontsize=10)
+    else:
+        plt.text(box1.peeling_trajectory.T[i].coverage-.03,box1.peeling_trajectory.T[i].density-.02 ,s,fontsize=10)
+plt.show()
+#Choose the last point
+i1=len(box1.peeling_trajectory.T.columns)-1
+box1.inspect(i1)
+box1.inspect(i1, style='graph')
+plt.show()
+ax=box1.show_pairs_scatter(i1)
+
+plt.show()
+
+#Find 2nd box
+box2 = prim_alg.find_box()
+#Visualizations of Box2
+box2.show_tradeoff()
+for i in range(0,len(box2.peeling_trajectory.T.columns)):
+    s=box2.peeling_trajectory.T[i].id
+    plt.text(box2.peeling_trajectory.T[i].coverage,box2.peeling_trajectory.T[i].density,s)
+#Choose the last point
+i2=len(box2.peeling_trajectory.T.columns)-1
+box2.inspect(i2)
+box2.inspect(i2,style='graph')
+plt.show()
+box2.show_pairs_scatter(i2)
+plt.show()
+
+coverage_2boxes=box1.coverage+box2.coverage
